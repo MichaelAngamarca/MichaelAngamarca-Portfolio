3833c0e9f0177e998e60a48c7e85abbe0aed885c
Michael Angamarca <michaelangamarca558@gmail.com>
2024-09-21 00:13:25 -0400

    all

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MichaelAngamarca-Portfolio/",
+  base: "/ma-portfolio/",
   plugins: [react()],
   css:{
     modules:{
